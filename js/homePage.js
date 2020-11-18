e3fbf32625ac582d3c31dc0a6bcb208eadb0c879
utkarshmega <utkarsheduc@gmail.com>
2020-11-19 01:50:06 +0530

    Added tabular form using json object

--- js/homePage.js
+++ js/homePage.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-
+    
     const headerHtml = `
         <th></th>
         <th>Name</th>
@@ -12,19 +12,48 @@ const createInnerHtml = () => {
         <th>Salary</th>
         <th>Start Date</th>
         <th>Actions</th> `;
-const innerHtml = ` ${headerHtml}
-    <tr>
-        <td><img class = "profile" alt ="" src = "../assets/profile-images/Ellipse -2.png"></td>
-        <td>Utkarsh Agrawal</td>
-        <td>Male</td>
-        <td><div class = "dept-label">Engineer</div>
-            <div class = "dept-label">Finance</div></td>
-        <td>500000</td>
-        <td>15 Sep 2020</td>
-        <td>
-            <img id ="1" onclick="remove(this)" alt = "delete" src = "../assets/icons/delete-black-18dp.svg">
-            <img id ="1" onclick="update(this)" alt ="edit" src = "../assets/icons/create-black-18dp.svg">
-        </td>
-    </tr> `;
+
+        let empPayrollData = createEmployeePayrollDataJSON()[0];
+    const innerHtml = ` ${headerHtml}
+        <tr>
+            <td><img class = "profile" alt ="" src = "${empPayrollData._profilePic}" alt = ""></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td><div class = "dept-label">${empPayrollData._department[0]}</div>
+                <div class = "dept-label">${empPayrollData._department[1]}</div></td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img id ="1" onclick="remove(this)" alt = "delete" src = "../assets/icons/delete-black-18dp.svg">
+                <img id ="1" onclick="update(this)" alt ="edit" src = "../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr> `;
     document.querySelector('#table-display').innerHTML = innerHtml;
 };
+
+const createEmployeePayrollDataJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Utkarsh Agrawal' ,
+            _gender: 'Male',
+            _department: ['Engineer', 'Finance'],
+            _salary: '750000',
+            _startDate: '29 Oct 2020',
+            _note: 'HI',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -3.png'
+        },
+        {
+            _name: 'Shashank Singh' ,
+            _gender: 'Male',
+            _department: ['Finance'],
+            _salary: '510000',
+            _startDate: '15 July 2019',
+            _note: 'Hey',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -2.png'
+        }
+    ];
+    return empPayrollListLocal;
+}
+
