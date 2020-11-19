3ba517ca0145f9de15276b2c546c2f5cfa95ead0
utkarshmega <utkarsheduc@gmail.com>
2020-11-19 13:42:38 +0530

    ability of Delete image function added

--- js/homePage.js
+++ js/homePage.js
@@ -33,8 +33,8 @@ const createInnerHtml = () => {
             <td>${empPayrollData._salary}</td>
             <td>${empPayrollData._startDate}</td>
             <td>
-                <img id ="1" onclick="remove(this)" alt = "delete" src = "../assets/icons/delete-black-18dp.svg">
-                <img id ="1" onclick="update(this)" alt ="edit" src = "../assets/icons/create-black-18dp.svg">
+                <img id ="${empPayrollData._id}" onclick="remove(this)" alt = "delete" src = "../assets/icons/delete-black-18dp.svg">
+                <img id ="${empPayrollData._id}" onclick="update(this)" alt ="edit" src = "../assets/icons/create-black-18dp.svg">
             </td>
         </tr> `;
     }
@@ -49,4 +49,12 @@ const getDeptHtml = (departmentList) => {
     return departmentHtml;
   }
 
-
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node._id);
+    if(!empPayrollData) return;
+    const index = empPayrollList.map(empData => empData._id).indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+}
